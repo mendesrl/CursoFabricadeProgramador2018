bf8f9fe2fc1eaaa0d259f19183d0ef0b2ba5b2ba
mendesrl <ribeiro.larissamendes@gmail.com>
2019-02-14 21:02:01 -0300

    netflix

---
+++ Trabalho-Netflix/src/components/IframeVideo.vue
@@ -0,0 +1,39 @@
+<template>
+    <iframe
+      :src="idItem"
+      :width="tam" 
+      :height="tam" 
+      :frameborder="frame" 
+      :allow="allow" 
+      :allowfullscreen="allowfullscreen"
+    />
+</template>
+<script type="text/javascript">
+export default {
+  name: 'IframeVideo',
+  data: () => ({}),
+  props: {
+    idItem: {
+      type: String,
+      default: () => ('')
+    },
+    tam: {
+      type: String,
+      default: () => ('')
+    },
+    frame: {
+      type: String,
+      default: () => ('')
+    },
+    allow: {
+      type: String,
+      default: () => ('')
+    },
+    allowfullscreen: {
+      type: Number,
+      default: () => ('')
+    },
+  },
+  computed: {} 
+}
+</script>
---
+++ Trabalho-Netflix/src/components/ImgThumbnail.vue
@@ -0,0 +1,25 @@
+<template>
+        <img
+            :src="idItem"
+            :width="tam" 
+            :height="tam" 
+        />
+</template>
+<script type="text/javascript">
+export default {
+  name: 'ImgThumbnail',
+  data: () => ({}),
+  props: {
+      idItem: {
+      type: String,
+      default: () => ('')
+    },
+    tam: {
+      type: String,
+      default: () => ('')
+    }
+  },
+  computed: {} 
+}
+</script>
+
